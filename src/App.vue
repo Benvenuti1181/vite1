c0973ea97e059a31c6c32d2a1c6f6c64d6be4457
gabry <g.benvenuti58@gmail.com>
2023-05-24 15:51:50 +0200

    vite 1 -primo commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+
+import AppTitle from "./components/AppTitle.vue";
+
+export default {
+  components: {
+    AppTitle,
+  },
+  data() {
+    return {
+    };
+  },
+};
+
+</script>
+
+<template>
+  <div class=" primo">
+    <AppTitle />
+    <h1>eccoci </h1>
+    <AppTitle />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+  data() {
+    return {
+      message: "La mia prima app con Vite!",
+    };
+  },
+};
+
+</script>
+
+<template>
+  <h2>
+    {{ message }}
+  </h2>
+</template>
+
+<style scoped>
+h2 {
+  color: rgb(173, 9, 9);
+}
+</style>
